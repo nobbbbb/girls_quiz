ad3ec94ab169f4daf16b188358839f4cec0508d6
Takashi Nobusada <nobnobmh@gmail.com>
2021-03-27 21:30:57 +0900

    add: ヘッダーコンポーネントを作成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <router-view />
+  <v-app>
+    <Header />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import Header from './components/TheHeader'
 
 export default {
   name: 'App',
-
+  components: {
+		Header
+  },
   data: () => ({
     //
   }),
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+	<header>
+		<div class="text-center my-4">
+			<router-link to="/" class="title">
+				<span>{{ title }}</span>
+			</router-link>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "Header",
+	data() {
+		return {
+			title: "Girls Quiz"
+		}
+	}
+}
+</script>
+
+<style scoped>
+.title {
+	text-decoration: none;
+	color: white;
+	font-size: 1.5rem;
+}
+header {
+	background-color:  rgb(71, 187, 207);
+}
+</style>
