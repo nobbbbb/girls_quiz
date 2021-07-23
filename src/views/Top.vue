8836f21fbe998351a88d07408fc61e8aeea66ac2
Takashi Nobusada <nobnobmh@gmail.com>
2021-07-23 23:25:58 +0900

    トップ画面作成 #5

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,11 +1,43 @@
 <template>
-  <div class="top">
+  <div class="container">
+		<div class="row my-12">
+			<div class="app-title col-md-8">
+				<span class="first-title">Girls</span>
+				<br />
+				<span class="last-title">Quiz</span>
+			</div>
+		</div>
+			<div class="start-button my-12 py-5">
+				<div class="text-center">
+					<router-link 
+						to="/quiz" 
+						style="text-decoration: none"
+					>
+						<v-btn
+							class="ma-2"
+							elevation=2
+							outlined
+							x-large
+							rounded
+						>
+						Start
+						</v-btn>
+					</router-link>
+				</div>
+		</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 export default {
   name: 'Top'
 }
 </script>
+
+<style scoped>
+.app-title {
+	font-size: 6rem;
+	text-align: center;
+	font-family: 'Courier New', Courier, monospace;
+}
+</style>
