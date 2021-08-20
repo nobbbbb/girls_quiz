6c0614d94eb7c18f8a9c158df66f00171861d5f3
Takashi Nobusada <nobnobmh@gmail.com>
2021-08-20 22:19:01 +0900

    add: veutifyのカードコンポーネントを使用

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,12 +1,17 @@
 <template>
-	<div class="container col-md-10">
-		<div class="result my-12">
-			<h1>5問中{{ NumberResult }}問正解でした</h1>
+	<div class="container col-md-10 my-12">
+		<v-card
+			class="card py-10 my-5"
+			color="rgba(71, 187, 207, 0.2)"
+			outlined
+		>
+		<div class="result">
+			<h1>5問中{{ NumberResult }}問正解</h1>
 			<h1>「{{ result.charactor }}」に怒られてしまいました</h1>
 			<h1>理由は「{{ result.detail }}」でした</h1>
 		</div>
 
-		<div class="button">
+		<div class="button mt-8">
 			<v-btn
 				large
 				depressed
@@ -26,6 +31,7 @@
 				Twitterで呟く
 			</v-btn>
 		</div>
+		</v-card>
 	</div>
 </template>
 
